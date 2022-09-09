a473de669ec33a6335af317507bd965fa71bb5b4
Nhat Minh <67325822+nhatminhnagoya@users.noreply.github.com>
2022-09-09 17:35:06 +0900

    slick slider

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(function () {
+  $("#js-slider-3").slick({
+    arrows: true, // 前・次のボタンを表示する
+    dots: false, // ドットナビゲーションを表示する
+    appendDots: $(".dots-3"), // ドットナビゲーションの生成位置を変更
+    speed: 1000, // スライドさせるスピード（ミリ秒）
+    slidesToShow: 1, // 表示させるスライド数
+    centerMode: true, // slidesToShowが奇数のとき、現在のスライドを中央に表示する
+    variableWidth: true, // スライド幅の自動計算を無効化
+  });
+});
